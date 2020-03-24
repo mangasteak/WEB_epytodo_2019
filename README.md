06f82d62eb93602c8c8ee836f174a08ac2983428
Huntears <alexandre.flion@epitech.eu>
2020-03-24 12:29:47 +0100

    [ADD]: database usage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EpyToDo
+
+For now all the configuration has to be done in a **.env** file !
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
+import pymysql as sql
+import os
+from config import *
 
 @app.route("/", methods=["GET"])
 def route_index():
@@ -12,3 +15,20 @@ def route_user(username):
     return render_template("index.html",
                             title="Hello " + username,
                             myContent="My SUPER content for " + username + "!!!")
+
+@app.route("/user")
+def route_all_users():
+    result = ""
+    try:
+        connect = sql.connect(host=DATABASE_HOST,
+                              user=DATABASE_USER,
+                              passwd=DATABASE_PASS,
+                              db=DATABASE_NAME)
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM user")
+        result = cursor.fetchall()
+        cursor.close()
+        connect.close()
+    except Exception as e:
+        print("Caught an exception :", e)
+    return jsonify(result);
